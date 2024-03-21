f0714d829bc4c1630d35b82caa0121be20813e69
Jotacarma90 <jose.carreras@wazuh.com>
2024-03-21 19:10:37 +0100

    Modified windows generate package script and create workflow to use it

--- packages/windows/entrypoint.sh
+++ packages/windows/entrypoint.sh
@@ -5,14 +5,9 @@ set -ex
 BRANCH=$1
 JOBS=$2
 DEBUG=$3
-REVISION=$4
+ZIP_NAME=$4
 TRUST_VERIFICATION=$5
 CA_NAME=$6
-
-URL_REPO=https://github.com/wazuh/wazuh/archive/${BRANCH}.zip
-
-# Download the wazuh repository
-wget -O wazuh.zip ${URL_REPO} && unzip wazuh.zip
 
 # Compile the wazuh agent for Windows
 FLAGS="-j ${JOBS} IMAGE_TRUST_CHECKS=${TRUST_VERIFICATION} CA_NAME=\"${CA_NAME}\" "
@@ -21,15 +16,20 @@ if [[ "${DEBUG}" = "yes" ]]; then
     FLAGS+="-d "
 fi
 
-bash -c "make -C /wazuh-*/src deps TARGET=winagent ${FLAGS}"
-bash -c "make -C /wazuh-*/src TARGET=winagent ${FLAGS}"
+if [[ ${BRANCH} != "" ]]; then
+    URL_REPO=https://github.com/wazuh/wazuh/archive/${BRANCH}.zip
+    SRC_PATH=/wazuh-*
 
-rm -rf /wazuh-*/src/external
+    # Download the wazuh repository
+    wget -O wazuh.zip ${URL_REPO} && unzip wazuh.zip
+else
+    SRC_PATH=/local-src
+fi
 
-# Zip the compiled agent and move it to the shared folder
-short_commit_hash="$(curl -s https://api.github.com/repos/wazuh/wazuh/commits/${BRANCH} \
-                          | grep '"sha"' | head -n 1| cut -d '"' -f 4 | cut -c 1-7)"
-version="$(cat /wazuh-*/src/VERSION| cut -d 'v' -f 2)"
-ZIP_NAME="wazuh-agent_${version}-${REVISION}_windows_${short_commit_hash}.zip"
-zip -r ${ZIP_NAME} wazuh-*
+bash -c "make -C ${SRC_PATH}/src deps TARGET=winagent ${FLAGS}"
+bash -c "make -C ${SRC_PATH}/src TARGET=winagent ${FLAGS}"
+
+rm -rf ${SRC_PATH}/src/external
+
+zip -r ${ZIP_NAME} ${SRC_PATH}
 cp ${ZIP_NAME} /shared
